9361b391134c6c26c3850460b115c4089cd4025e
RoeyZadaAK <roy@arborknot.io>
2025-07-07 15:44:19 +0300

    dropdown final

--- src/UIKit/Elements/dropdown/Dropdown.jsx
+++ src/UIKit/Elements/dropdown/Dropdown.jsx
@@ -1,19 +1,74 @@
+import { useEffect, useRef, useState } from 'react';
 import { Icon } from '../Icon';
 import './dropdown.css';
 
-export const Dropdown = () => {
+export const Dropdown = ({ list, selected, onChange }) => {
+    const [isOpen, setIsOpen] = useState(true);
+    const wrapRef = useRef();
+
+    useEffect(() => {
+        document.body.addEventListener("click", handleClose);
+
+        return () => {
+            document.body.removeEventListener("click", handleClose);
+        }
+    }, [])
+
+    const handleClose = (e) => {
+        if (!wrapRef.current) return;
+
+        if (wrapRef.current.contains(e.target)) {
+            return;
+        }
+
+        setIsOpen(false);
+    }
+
+    const handleToggle = (e) => {
+        e.stopPropagation();
+        setIsOpen(!isOpen);
+    }
+
+    const renderTitle = () => {
+        if (selected) {
+            const item = list.find(item => item.key === selected);
+            if (item) {
+                return item.value;
+            }
+        }
+
+        return "please select"
+    }
+
+    const handleSelect = (item) => {
+        onChange(item.key);
+        setIsOpen(false);
+    }
+
+    const getItemStyle = (item) => {
+        if (item.key === selected) {
+            return "list_item selected"
+        }
+        return "list_item"
+    }
+
     return (
-        <div className="dropdown">
-            <div className="trigger">
-                <div className="selected_value">please selected</div>
+        <div className="dropdown" ref={wrapRef}>
+            <div className="trigger" onClick={handleToggle}>
+                <div className="selected_value">{renderTitle()}</div>
                 <Icon i="keyboard_arrow_down" />
             </div>
-            <ul className='list'>
-                <li className='list_item'>item1</li>
-                <li className='list_item'>item2</li>
-                <li className='list_item'>item3</li>
-                <li className='list_item'>item4</li>
-            </ul>
+            {isOpen && (
+                <ul className='list'>
+                    {list.map((item) => (
+                        <li
+                            key={item.key}
+                            className={getItemStyle(item)}
+                            onClick={() => handleSelect(item)}
+                        >{item.value}</li>
+                    ))}
+                </ul>
+            )}
         </div>
     )
 }
--- src/pages/dropdown/DropdownPage.jsx
+++ src/pages/dropdown/DropdownPage.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react"
 import { Dropdown } from "../../UIKit/Elements/dropdown/Dropdown"
 
+const list = [
+    {
+        key: 1,
+        value: "Mosh"
+    },
+    {
+        key: 2,
+        value: "David"
+    },
+    {
+        key: 3,
+        value: "Baruch"
+    },
+    {
+        key: 4,
+        value: "Ruth"
+    }
+]
 export const DropdownPage = () => {
+    const [selected, setSelected] = useState(3);
+
     return (
         <div>
             <h2>Dropdown page</h2>
-            <Dropdown />
+            <Dropdown list={list} selected={selected} onChange={setSelected} />
             <h2>iii</h2>
         </div>
     )
