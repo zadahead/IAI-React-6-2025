8e593faa695dc216499cf4f30992b9b49c22fa7f
RoeyZadaAK <roy@arborknot.io>
2025-07-07 13:11:43 +0300

    ref html

---
+++ src/components/refHtml/refHTML.jsx
@@ -0,0 +1,9 @@
+export const RefHTML = () => {
+    return (
+        <div>
+            <h2>Ref HTML</h2>
+            <input />
+        </div>
+    )
+}
+
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,11 +1,12 @@
 import { RefExample } from "../../components/refExample/RefExample"
+import { RefHTML } from "../../components/refHtml/refHTML"
 
 export const About = () => {
     return (
         <div>
             <h2>This is our about page</h2>
             <div className="mt-2">
-                <RefExample />
+                <RefHTML />
             </div>
         </div>
     )
