d2cb4073dc68f2ee65f09d81ef46b030355b4d07
RoeyZadaAK <roy@arborknot.io>
2025-07-14 08:35:53 +0300

    counter hook

---
+++ src/components/counterHook/CounterHook.jsx
@@ -0,0 +1,26 @@
+import { useCounter } from "../../hooks/useCounter";
+import { Btn } from "../../UIKit/Elements/btn/Btn"
+
+/*
+    Custom hook: 
+
+    1) separate logic from render
+    2) create a hook function starting with "useXXX" and paste login there
+    3) return the values needed for the rendering of the component
+    4) call custom hook from component and get the values needed for it to run
+    5) move new custom hook to it own location
+*/
+
+
+export const CounterHook = () => {
+    //logic
+    const { count, handleAdd } = useCounter();
+
+    //render
+    return (
+        <div>
+            <h2>Counter Hook, {count}</h2>
+            <Btn onClick={handleAdd}>Add</Btn>
+        </div>
+    )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import { ColorSwitcher } from "../../components/colorSwitcher/ColorSwitcher";
 import { Counter } from "../../components/counter/Counter";
+import { CounterHook } from "../../components/counterHook/CounterHook";
 import { LanguagePicker } from "../../components/languagePicker/LanguagePicker";
 
 
@@ -8,7 +9,7 @@ export const Home = () => {
     return (
         <div>
             <h2>This is our homepage</h2>
-            <LanguagePicker />
+            <CounterHook />
         </div>
     )
 }
