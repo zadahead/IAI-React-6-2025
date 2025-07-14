2fc7bb4e8371e7147fd8b1337aaf6ee8e27db129
RoeyZadaAK <roy@arborknot.io>
2025-07-14 14:09:17 +0300

    count

--- src/components/counterRedux/CounterRedux.jsx
+++ src/components/counterRedux/CounterRedux.jsx
@@ -1,9 +1,10 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { Btn } from "../../UIKit/Elements/btn/Btn"
 import { handleAdd } from "../../store/slices/counterSlice";
 
 export const CounterRedux = () => {
     const dispatch = useDispatch();
+    const count = useSelector((state) => state.counter.value);
 
     const handleCounterAdd = () => {
         dispatch(handleAdd())
@@ -11,6 +12,7 @@ export const CounterRedux = () => {
 
     return (
         <div>
+            <h2>count, {count}</h2>
             <Btn onClick={handleCounterAdd}>Add</Btn>
         </div>
     )
